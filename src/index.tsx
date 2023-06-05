a9bbe9b697a227d63f59d8554a0062a961fddfef
TheNolle <github@thenolle.com>
2023-06-05 19:09:31 +0200

    Cleaned repository

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 
 import './index.scss'
 
@@ -13,9 +12,7 @@ if (root) {
         .createRoot(root)
         .render(
             <React.StrictMode>
-                <BrowserRouter>
-                    <App />
-                </BrowserRouter>
+                <App />
             </React.StrictMode>
         )
 }
